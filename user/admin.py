1630425d3b34181237cdc6e20e225748277b5872
zyp <1558255789@qq.com>
2020-01-18 16:58:04 +0800

    add migrate file

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,6 @@ from user import models
 # Register your models here.
 
 
-admin.site.register(models.Account)
 admin.site.register(models.Administrator)
 admin.site.register(models.Student)
 admin.site.register(models.Teacher)
