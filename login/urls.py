ecff03ef25e626e43695fab23a7610b1ea6bdb17
zyp <1558255789@qq.com>
2020-01-18 17:54:16 +0800

    add login page

---
+++ login/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .views import LoginView
+
+
+urlpatterns = [
+    url(r'^$', LoginView.as_view(), name='login')
+]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+
+class LoginView(View):
+    """
+    登录视图
+    """
+    def get(self, request):
+        return render(request, 'login.html')
+
+
